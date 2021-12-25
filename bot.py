1959ecda582c022b5d3d93bc1d4e76385b10c34e
Anastasia Afanasyeva <afnastya1704@gmail.com>
2021-12-25 21:54:00 +0300

    Fix code style

--- bot.py
+++ bot.py
@@ -12,7 +12,10 @@ memes_number = len(os.listdir('memes'))
 
 @bot.message_handler(commands=['start'])
 def start_bot(message):
-    bot.send_message(message.chat.id, "Hi there! Click the button below!", reply_markup=meme_markup())
+    bot.send_message(
+        message.chat.id,
+        "Hi there! Click the button below!",
+        reply_markup=meme_markup())
 
 
 @bot.message_handler(func=lambda msg: msg.text == "Get Meme!", content_types=["text"])
