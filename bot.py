8bcb708caa66e325b192fca714c1b93a287e4755
Anastasia Afanasyeva <afnastya1704@gmail.com>
2021-12-25 20:38:19 +0300

    Add reply keyboard

--- bot.py
+++ bot.py
@@ -1,16 +1,32 @@
 import os
 import random
 import telebot
+from telebot import types
 
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
 memes_number = len(os.listdir('memes'))
 
-@bot.message_handler(content_types=["text"])
+
+
+@bot.message_handler(commands=['start'])
+def start_bot(message):
+    bot.send_message(message.chat.id, reply_markup=meme_markup())
+
+
+@bot.message_handler(func=lambda msg: msg.text == "Get Meme!", content_types=["text"])
 def send_meme(message):
     meme_id = random.randint(1, memes_number)
     with open(f'memes/meme{meme_id}.jpg', 'rb') as meme:
-        bot.send_photo(message.chat.id, meme)
+        bot.send_photo(
+            message.chat.id,
+            meme,
+            reply_markup=meme_markup())
+
+
+def meme_markup():
+    return types.ReplyKeyboardMarkup(types.KeyboardButton("Get Meme!"))
+
 
 bot.polling(none_stop=True)
