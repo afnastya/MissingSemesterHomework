53915e349e508b93ebb54cbca02b9a3dce07e45d
Anastasia Afanasyeva <afnastya1704@gmail.com>
2021-12-25 17:20:41 +0300

    Return to echo bot

--- bot.py
+++ bot.py
@@ -4,11 +4,12 @@ import telebot
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
-# def echo_messages(message):
-#     bot.send_message(message.chat.id, message.text)
-
 @bot.message_handler(content_types=["text"])
-def send_meme(message):
-    bot.send_photo(message.chat.id, "memes/meme1.jpg")
+def echo_messages(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+# def send_meme(message):
+#     bot.send_photo(message.chat.id, "memes/meme1.jpg")
 
 bot.polling(none_stop=True)
