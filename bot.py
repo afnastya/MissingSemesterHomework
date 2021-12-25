f70ee166cbbc046eda83f1a359b5e7aed2cb12fb
Anastasia Afanasyeva <afnastya1704@gmail.com>
2021-12-25 18:49:26 +0300

    Fix code style

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
-import telebot
 import random
+import telebot
 
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
@@ -10,6 +10,7 @@ memes_number = len(os.listdir('memes/'))
 @bot.message_handler(content_types=["text"])
 def send_meme(message):
     meme_id = random.randint(1, memes_number)
-    bot.send_photo(message.chat.id, open('memes/meme{0}.jpg'.format(meme_id), 'rb'))
+    with open(f'memes/meme{meme_id}.jpg', 'rb') as meme:
+        bot.send_photo(message.chat.id, meme)
 
 bot.polling(none_stop=True)
