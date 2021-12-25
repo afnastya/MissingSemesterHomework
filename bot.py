29e679d31f008c4111177b9f23dcda54abad14fb
Anastasia Afanasyeva <afnastya1704@gmail.com>
2021-12-25 18:41:18 +0300

    Add sending random meme

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 import os
 import telebot
+import random
 
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
-# def echo_messages(message):
-#     bot.send_message(message.chat.id, message.text)
+memes_number = len(os.listdir('memes/'))
 
 @bot.message_handler(content_types=["text"])
 def send_meme(message):
-    bot.send_photo(message.chat.id, "memes/meme1.jpg")
+    meme_id = random.randint(1, memes_number)
+    bot.send_photo(message.chat.id, open('memes/meme{0}.jpg'.format(meme_id), 'rb'))
 
 bot.polling(none_stop=True)
