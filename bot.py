2decde7dc53099bc326389c4abf4a1012be7a0e1
Anastasia Afanasyeva <afnastya1704@gmail.com>
2021-12-25 05:11:29 +0300

    Add bug:)

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(TELEGRAM_TOKEN)
 @bot.message_handler(content_types=["text"])
 def echo_messages(message):
     if message.from_user.username != "afnastya":
-        return
+        return;
     bot.send_message(message.chat.id, message.text)
 
 bot.polling(none_stop=True)
