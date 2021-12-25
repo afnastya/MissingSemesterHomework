5616b9b59ffa994b310d1b332956b16bd217e8c9
Anastasia Afanasyeva <afnastya1704@gmail.com>
2021-12-25 17:39:09 +0300

    Return to sending a meme

--- bot.py
+++ bot.py
@@ -4,12 +4,11 @@ import telebot
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
+# def echo_messages(message):
+#     bot.send_message(message.chat.id, message.text)
+
 @bot.message_handler(content_types=["text"])
-def echo_messages(message):
-    bot.send_message(message.chat.id, message.text)
-
-
-# def send_meme(message):
-#     bot.send_photo(message.chat.id, "memes/meme1.jpg")
+def send_meme(message):
+    bot.send_photo(message.chat.id, "memes/meme1.jpg")
 
 bot.polling(none_stop=True)
